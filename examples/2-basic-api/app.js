20c3390ce82542386bf63abe146b784373c1c608
shadiqueali <sadikali322@gmail.com>
2022-01-26 19:59:26 +0530

    first commit

---
+++ examples/2-basic-api/app.js
@@ -0,0 +1,26 @@
+const result= document.querySelector('.result')
+
+
+const fetchData = async () => {
+    try {
+        const {data} = await axios.get('/api/2-basic-api')
+        const produtcts = data.map((product)=>{
+            const {image:{url},name,price} = product
+            return ` <article class="product">
+            <img
+              src="${url}"
+              alt="${name}"
+            />
+            <div class="info">
+              <h5>${name}</h5>
+              <h5 class="price">$${price}</h5>
+            </div>
+          </article> `
+        }).join('')
+        result.innerHTML =produtcts
+    } catch (error) {
+        result.innerHTML =`<h4>there was an error, try again</h4>`
+    }
+}
+
+fetchData()
